8713677e44116a22d5cbe46a3192d333e180a85a
pandaachuidc <53323186+pandaachuidc@users.noreply.github.com>
2020-07-20 15:24:56 +0800

    完成首頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,15 @@
-$(document).ready(() => {
-  console.log('Hello Bootstrap4');
+let zoomInLightBtn = document.querySelector(".zoomInLightBtn");
+let zoomInLight = document.querySelector(".zoomInLight");
+
+zoomInLightBtn.addEventListener("click", function () {
+  zoomInLight.style.display = "block";
+  zoomInLight.classList.add("active");
+  setTimeout(function () {
+    gotoStep1Page();
+  }, 1000);
 });
+
+
+var gotoStep1Page = function () {
+  window.location.href="step1.html";
+};
