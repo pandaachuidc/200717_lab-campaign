49ce0a9f32ccc7ac41a528f421429fb796e83b10
pandaachuidc <53323186+pandaachuidc@users.noreply.github.com>
2020-07-20 15:23:48 +0800

    Update 2020-07-20T07:23:40.503Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,16 @@
 "use strict";
 
-$(document).ready(function () {
-  console.log('Hello Bootstrap4');
+var zoomInLightBtn = document.querySelector(".zoomInLightBtn");
+var zoomInLight = document.querySelector(".zoomInLight");
+zoomInLightBtn.addEventListener("click", function () {
+  zoomInLight.style.display = "block";
+  zoomInLight.classList.add("active");
+  setTimeout(function () {
+    gotoStep1Page();
+  }, 1000);
 });
+
+var gotoStep1Page = function gotoStep1Page() {
+  window.location.href = "step1.html";
+};
 //# sourceMappingURL=all.js.map
