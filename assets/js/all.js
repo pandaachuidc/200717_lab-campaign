1c5158ffd12cacd4b8e351d4d38b7decff14e2ef
pandaachuidc <53323186+pandaachuidc@users.noreply.github.com>
2020-07-21 18:46:15 +0800

    Update 2020-07-21T10:46:00.526Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,18 @@
 "use strict";
 
+// Animation
+gsap.to("#square", {
+  attr: {
+    x: 0
+  },
+  duration: 2,
+  repeat: 0
+});
 var zoomInLightBtn = document.querySelector(".zoomInLightBtn");
 var zoomInLight = document.querySelector(".zoomInLight");
 zoomInLightBtn.addEventListener("click", function () {
   zoomInLight.style.display = "block";
-  zoomInLight.classList.add("active");
+  zoomInLight.classList.add("a-zoomInLightShow");
   setTimeout(function () {
     gotoStep1Page();
   }, 1000);
@@ -12,5 +20,27 @@ zoomInLightBtn.addEventListener("click", function () {
 
 var gotoStep1Page = function gotoStep1Page() {
   window.location.href = "step1.html";
-};
+}; // slick
+
+
+$(document).ready(function () {
+  $(".sliderMain").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    onAfterChange: function onAfterChange() {
+      player.stopVideo();
+    }
+  });
+  var video = $(".sliderMain .slick-active").find("video").get(0).play();
+  $(".sliderMain").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+    $(".sliderMain .slick-slide").find("video").get(0).pause();
+    var video = $(".sliderMain .slick-active").find("video").get(0).play();
+  });
+});
 //# sourceMappingURL=all.js.map
